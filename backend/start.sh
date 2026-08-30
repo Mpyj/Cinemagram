88a1f44bf8d9403f8da358ceb24ae5622bc7d214
parsa <mohammad.sahar1388@gmail.com>
2026-08-30 21:43:44 +0330

    fix start script again

--- backend/start.sh
+++ backend/start.sh
@@ -3,6 +3,12 @@
 set -e
 
 echo "======================================"
+echo "0. Creating required directories..."
+echo "======================================"
+
+mkdir -p uploads
+
+echo "======================================"
 echo "1. Creating database tables..."
 echo "======================================"
 
@@ -10,13 +16,11 @@ python -c "import app.main"
 
 echo "Database tables created successfully."
 
-
 echo "======================================"
 echo "2. Creating Owner..."
 echo "======================================"
 
 python make_owner.py
-
 
 echo "======================================"
 echo "3. Importing Movies..."
@@ -24,20 +28,17 @@ echo "======================================"
 
 python import_from_tmdb.py movies_list.txt --list
 
-
 echo "======================================"
 echo "4. Importing Series..."
 echo "======================================"
 
 python import_from_tmdb.py series_list.txt --list
 
-
 echo "======================================"
 echo "5. Importing Anime..."
 echo "======================================"
 
 python import_anime.py anime_list.txt --list
-
 
 echo "======================================"
 echo "6. Starting Cinemagram API..."
